e48363d1cbfaa9d7372648bfb4b8f562b8faeb69
Kevin Wasike Wakhisi <kevin@tawalabora.space>
2025-05-27 23:40:50 +0300

    updates

--- init.sh
+++ init.sh
@@ -71,7 +71,7 @@ export USERNAME PASSWORD SUDO_PRIVILEGES CODE_SERVER_PORT
 
 # Run setup modules
 download_and_run() {
-    sudo curl -fsSL "$BASE_URL/$1"
+    curl -fsSL "$BASE_URL/$1" | sudo bash
 }
 
 download_and_run "user-setup.sh"
