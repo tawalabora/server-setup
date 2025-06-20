662096f02008c97e8a3f5f643998c8d33bf2f856
Kevin Wasike Wakhisi <kevin@tawalabora.space>
2025-06-20 21:52:07 +0300

    Update github.sh

--- user/scripts/github.sh
+++ user/scripts/github.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Setup GitHub folder
-if [ ! -d "/home/$USER/github" ]; then
-  echo "Creating /home/$USER/github directory..."
-  mkdir -p "/home/$USER/github"
+if [ ! -d "/home/$USER/repos" ]; then
+  echo "Creating /home/$USER/repos directory..."
+  mkdir -p "/home/$USER/repos"
 fi
 
-chmod -R 700 "/home/$USER/github"
-chown -R "$USER:$USER" "/home/$USER/github"
+chmod -R 700 "/home/$USER/repos"
+chown -R "$USER:$USER" "/home/$USER/repos"
