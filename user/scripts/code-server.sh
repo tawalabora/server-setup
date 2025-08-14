3927ae74621985d514c073ab91f5c36db6533be0
Kevin Wasike Wakhisi <kevin@tawalabora.space>
2025-08-14 16:06:45 +0300

    Update code-server.sh
    Access will be via ssh key through ssh forwarding

--- user/scripts/code-server.sh
+++ user/scripts/code-server.sh
@@ -4,8 +4,7 @@ set -e
 mkdir -p "/home/$USER/.config/code-server"
 cat <<EOF > "/home/$USER/.config/code-server/config.yaml"
 bind-addr: 127.0.0.1:$CODE_SERVER_PORT
-auth: password
-password: $PASSWORD
+auth: none
 cert: false
 EOF
 
