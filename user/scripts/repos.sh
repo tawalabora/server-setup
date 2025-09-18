0caf59bf4bbe625f9f38e2438ec1af523a7a7cf6
Kevin Wasike Wakhisi <kevin@tawalabora.space>
2025-09-18 13:34:02 +0300

    update repos.sh

--- user/scripts/repos.sh
+++ user/scripts/repos.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-TARGET_DIR="/home/$USER/repos"
-
-# Create the directory if it doesn't exist
-if [ ! -d "$TARGET_DIR" ]; then
-  sudo mkdir -p "$TARGET_DIR"
-fi
+mkdir -p "$HOME/repos"
