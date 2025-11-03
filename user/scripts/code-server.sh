bd7cf16cf248f3da2de59a3e20b8180203dd42f3
Kevin Wasike Wakhisi <kevin@tawalabora.space>
2025-11-03 10:26:36 +0300

    Ensure /tmp/code-server-port exists before writing per-user port file

--- user/scripts/code-server.sh
+++ user/scripts/code-server.sh
@@ -48,6 +48,9 @@ RANDOM_PASS=$(openssl rand -base64 12)
 echo "$RANDOM_PASS" > "$TMP_DIR/code-server-pass.tmp"
 echo "$PORT" > "$TMP_DIR/code-server-port.tmp" # This is for internal 'user' setup use only,
 
+# Create directory if it doesn't exist
+mkdir -p "/tmp/code-server-port"
+
 if ! echo "$PORT" > "/tmp/code-server-port/$USER.tmp"; then
     echo -e "${RED}❌ Failed to write port number to temporary file${NC}"
     exit 1
